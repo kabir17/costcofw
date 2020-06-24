7f91b7f37e4a447950c25e9f0556b9203dc1d5ed
mkabir <kajol11373@gmail.com>
2020-06-24 17:16:26 -0400

    Invalid ID

--- Costco/src/main/java/homepage/LogInPage.java
+++ Costco/src/main/java/homepage/LogInPage.java
@@ -3,6 +3,9 @@ package homepage;
 public class LogInPage {
 
     public void signIn(){
-        System.out.println("I cant sign in");
+        System.out.println("I cant sign ini");
+    }
+    public void invalidId(){
+        System.out.println("Invalid ID");
     }
 }
