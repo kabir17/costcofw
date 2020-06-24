8e06a39e63fd751af789296c5f1578d37a640f86
mkabir <kajol11373@gmail.com>
2020-06-24 17:23:44 -0400

    delete LogIn

--- Costco/src/main/java/homepage/LogInPage.java
+++ Costco/src/main/java/homepage/LogInPage.java
@@ -1,11 +1,5 @@
 package homepage;
 
 public class LogInPage {
-
-    public void signIn(){
-        System.out.println("I cant sign ini");
-    }
-    public void invalidId(){
-        System.out.println("Invalid ID");
-    }
+    
 }
