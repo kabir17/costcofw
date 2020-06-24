89da9bfa5b26062173d453002ba31dbdc0ca8e2d
mkabir <kajol11373@gmail.com>
2020-06-24 17:25:03 -0400

    added Login

--- Costco/src/main/java/homepage/LogInPage.java
+++ Costco/src/main/java/homepage/LogInPage.java
@@ -1,5 +1,11 @@
 package homepage;
 
 public class LogInPage {
-    
+
+    public void signIn(){
+        System.out.println("I cant sign ini");
+    }
+    public void invalidId(){
+        System.out.println("Invalid ID");
+    }
 }
